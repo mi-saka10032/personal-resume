b80e572d47171377811fc7868b25feaf5bce18cf
yuzhihang <misaka10032@aliyun.com>
2023-02-05 20:04:07 +0800

    init

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export default {
+  title: "[简历]余志航-前端开发工程师",
+};
---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="astro/client" />
+
+type Info = { label: string; value: string };
+
+type Contact = Info & { link: boolean };
+
+type Skill = { label: string; progress: number };
+
+type Work = {
+  title: string;
+  startTime: string;
+  endTime: string;
+  desc?: Array<string | string[]>;
+};
+
+type Project = Work & {
+  demoLink?: string;
+  skill: string;
+  background: string;
+  result: string;
+};
